4fc8a5ee2ee587548dd02adda13092c19bc6822f
nickmasonswe <nickmasonswe@gmail.com>
2023-12-05 20:51:02 -0500

    pair program nickd finish define mongoDB schema

--- server/models/city.js
+++ server/models/city.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const citySchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
+  thingsToDo: [String],
+  thingsNotToDo: [String],
+  recommendations: {
+    type: String,
+  },
+  created_at: { type: Date, default: Date.now },
 });
 
 const City = mongoose.model('City', citySchema);
